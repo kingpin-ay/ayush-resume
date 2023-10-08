4c166bb193038979c2b8b3989dfc79fd9fd77366
kingpin-ay <ayushmondal001@gmail.com>
2023-10-08 21:18:09 +0530

    Added the Basic Home page
    The basic Home page for the portfolio is added and now working perfectly

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,3 @@
-
 import About from "./Pages/About";
 import Blog from "./Pages/Blog";
 import Contacts from "./Pages/Contacts";
@@ -24,15 +23,73 @@ const navigationData = [
   },
 ];
 
-const firstName: string = "Ayush";
+const ToolsLogo: { url: string; altText: string }[] = [
+  {
+    url: "https://cdn4.iconfinder.com/data/icons/logos-3/600/React.js_logo-48.png",
+    altText: "React Logo",
+  },
+  {
+    url: "https://upload.wikimedia.org/wikipedia/commons/4/4c/Typescript_logo_2020.svg",
+    altText: "Typescript Logo",
+  },
+  {
+    url: "https://cdn3.iconfinder.com/data/icons/teenyicons-outline-vol-2/15/nextjs-48.png",
+    altText: "Next Js Logo",
+  },
+  {
+    url: "https://cdn4.iconfinder.com/data/icons/iconsimple-logotypes/512/github-48.png",
+    altText: "Github Logo",
+  },
+  {
+    url: "https://upload.wikimedia.org/wikipedia/commons/9/98/Solidity_logo.svg",
+    altText: "Solidity Logo",
+  },
+  {
+    url: "https://static-00.iconduck.com/assets.00/apps-neovim-icon-512x512-w4ecv3uh.png",
+    altText: "Neovim Logo",
+  },
+  {
+    url: "https://static-00.iconduck.com/assets.00/apps-alacritty-icon-512x512-be0xnta9.png",
+    altText: "Alacritty Logo",
+  },
+  {
+    url: "https://cdn3.iconfinder.com/data/icons/social-media-2169/24/social_media_social_media_logo_docker-48.png",
+    altText: "Docker Logo",
+  },
+  
 
+];
+
+const firstName: string = "Ayush";
+const profession: string = "Full Stack BlockChain Developer";
+const expertise: string[] = [
+  "Html",
+  "Css",
+  "JavaScript",
+  "TypeScript",
+  "Solidty",
+  "python",
+  "Node",
+  "Nestjs",
+];
+const myField: string = "BlockChain";
+const myApproach : {workPrinciple : string , approach: string} = {
+  workPrinciple: "Consistency",
+  approach: "Divide-and-conquer"
+}
 
 function App() {
-
   return (
     <>
       <Navbar navigationData={navigationData} />
-      <Home firstName={firstName}/>
+      <Home
+        myApproach={myApproach}
+        firstName={firstName}
+        profession={profession}
+        expertise={expertise}
+        myField={myField}
+        usedToolsLogo={ToolsLogo}
+      />
       <About />
       <Contacts />
       <Blog />
--- src/Pages/Home/Home.tsx
+++ src/Pages/Home/Home.tsx
@@ -1,39 +1,88 @@
-import { motion } from "framer-motion";
 import style from "./Home.module.css";
 import dp from "../../assets/DP.jpg";
+import ReavealAnimation from "../../animationLayouts/RevealAnimation";
 
-
-
-const Home = (props : Props) => {
+const Home = (props: Props) => {
   return (
-    <motion.div id="home" className={style.container}>
-      <motion.div className={style.banner}>
-        <motion.img
-          src={dp}
-          alt="My Image"
-          className={style.dpHolder}
-          initial={{ y: 200, scale: 0 }}
-          animate={{ y: 0, scale: 1 }}
-          transition={{ type: "spring", duration: 0.3 }}
-        />
-        <motion.h1
-          className={style.title}
-          initial={{ y: 200, scale: 0 }}
-          animate={{ y: 0, scale: 1 }}
-          transition={{ type: "spring", delay: 0.2, duration: 0.3 }}
-        >
-          Welcome To <br />
-          <span className={style.siteOwnerName}>{props.firstName}</span>'s Website
-        </motion.h1>
-      </motion.div>
-    </motion.div>
+    <div id="home" className={style.container}>
+      <ReavealAnimation>
+        <div className={style.banner}>
+          <img src={dp} alt="My Image" className={style.dpHolder} />
+          <h1 className={style.title}>
+            Welcome To <br />
+            <span className={style.siteOwnerName}>{props.firstName}</span>'s
+            Website
+          </h1>
+        </div>
+      </ReavealAnimation>
+
+      <div>
+        <ReavealAnimation>
+          <p className={style.aboutSection}>
+            I am a passionate{" "}
+            <span className={style.myWorkText}>{props.profession}</span> with a
+            strong dedication to{" "}
+            {props.expertise.map((eachExpertise, index) => (
+              <>
+                <span className={style.expertise} key={index}>
+                  {eachExpertise}
+                </span>
+                {" ,"}
+              </>
+            ))}
+            My journey in <span className={style.field}>{props.myField}</span>{" "}
+            has been a thrilling ride, filled with creativity, innovation, and a
+            relentless pursuit of excellence.
+          </p>
+        </ReavealAnimation>
+        <ReavealAnimation>
+          <div className={style.toolsContainer}>
+            <section>
+              <p>Tools That I use</p>
+            </section>
+            <div className={style.toolsImageContainer}>
+              {props.usedToolsLogo.map((eachTool, index) => {
+                return (
+                  <div key={index}>
+                    <img
+                      className={style.toolImage}
+                      src={eachTool.url}
+                      alt={eachTool.altText}
+                    />
+                  </div>
+                );
+              })}
+            </div>
+          </div>
+        </ReavealAnimation>
+        <ReavealAnimation>
+          <p className={style.aboutSection}>
+            I believe in the power of{" "}
+            <span className={style.myWorkText}>
+              {props.myApproach.workPrinciple}
+            </span>
+            . With a keen eye for detail and a knack for problem-solving, I
+            approach every project with
+            <span className={style.expertise}>
+              {" "}
+              {props.myApproach.approach}
+            </span>
+            . My goal is not just to meet your expectations but to exceed them,
+            crafting experiences that leave a lasting impact.
+          </p>
+        </ReavealAnimation>
+      </div>
+    </div>
   );
 };
 
-type Props ={
+type Props = {
   firstName: string;
-}
-
-
+  profession: string;
+  expertise: string[];
+  myField: string;
+  usedToolsLogo: { url: string; altText: string }[];
+  myApproach: { workPrinciple: string; approach: string };
+};
 
 export default Home;
